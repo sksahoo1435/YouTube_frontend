bd1eace06d55cde4f454297e9ee377ee9f8b68b5
sunil-sahoo-epsoft <ssahoo@eplanetsoft.com>
2024-11-19 21:29:04 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set base path
   build: {
     outDir: 'dist'
-  },
-  server: {
-    port: 3000 // Optional: Specify local development port
   }
-});
+})
